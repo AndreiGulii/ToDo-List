243f71f4daccea68de4a46c9fc67631020fbcf60
AndreiGulii <andreigulii1985@gmail.com>
2025-07-18 21:46:10 +0200

    Added Date formate for Date

--- Task.jsx
+++ Task.jsx
@@ -4,13 +4,20 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const tasks = [
   {
     id: 1,
-    task: "Task to do",
-    date: "18.07.2025",
-    info: "Info "
+    task: "Edit this Task",
+    date: new Date(2025, 6, 18),
+    info: "Edit this Info ",
   },
 ];
 
-const Task = ({ task, date, info, handleEditTask, handleDeleteTask, index }) => {
+const Task = ({
+  task,
+  date,
+  info,
+  handleEditTask,
+  handleDeleteTask,
+  index,
+}) => {
   const [isEdit, setIsEdit] = React.useState(false);
 
   const taskRef = React.useRef();
@@ -18,19 +25,19 @@ const Task = ({ task, date, info, handleEditTask, handleDeleteTask, index }) => 
   const infoRef = React.useRef();
 
   const handleClickSave = () => {
-    handleEditTask(index, taskRef.current.value, dateRef.current.value, infoRef.current.value);
+    handleEditTask(
+      index,
+      taskRef.current.value,
+      new Date(dateRef.current.value),
+      infoRef.current.value
+    );
     setIsEdit(false);
   };
 
   return isEdit ? (
-    <div className="input-group">
-      <input
-        className="ms-3"
-        ref={taskRef}
-        type="text"
-        defaultValue={task}
-      />
-      <input ref={dateRef} type="text" defaultValue={date} />
+    <div className="input-group align-items-center gap-2 ms-3">
+      <input className="ms-3" ref={taskRef} type="text" defaultValue={task} />
+      <input ref={dateRef} type="date" defaultValue={date.toISOString().slice(0, 10)} />
       <input ref={infoRef} type="text" defaultValue={info} />
       
       <button onClick={handleClickSave} className="btn btn-success">
@@ -40,7 +47,7 @@ const Task = ({ task, date, info, handleEditTask, handleDeleteTask, index }) => 
   ) : (
     <>
       <p className="mt-3 ms-3">{task}</p>
-      <p className="mt-3 ms-3">{date}</p>
+      <p className="mt-3 ms-3">{date.toLocaleDateString()}</p>
       <p className="mt-3 ms-3">{info}</p>
       <button onClick={() => setIsEdit(true)} className="btn btn-warning ms-3">
         Edit
@@ -54,12 +61,13 @@ const Task = ({ task, date, info, handleEditTask, handleDeleteTask, index }) => 
 
 const TaskList = () => {
   const [listOfTasks, setListOfTasks] = React.useState(tasks);
+
   const handleEditTask = (index, newTask, newDate, newInfo) => {
     const copyListOfTasks = [...listOfTasks];
 
     copyListOfTasks[index].task = newTask;
     copyListOfTasks[index].date = newDate;
-    copyListOfTasks[index].info = newInfo
+    copyListOfTasks[index].info = newInfo;
 
     setListOfTasks(copyListOfTasks);
   };
@@ -68,14 +76,14 @@ const TaskList = () => {
   };
   const handleAddTask = () => {
     const newTask = {
-      id: listOfTasks.length,
+      id: Date.now(),
       task: "New Task",
-      date: "New Date",
+      date: new Date(),
       info: "New Info",
     };
-    setListOfTasks([...listOfTasks, newTask, newDate, newInfo]);
+    setListOfTasks([...listOfTasks, newTask]);
   };
-
+    
   return (
     <>
       {listOfTasks.map(({ id, task, date, info }, index) => (
@@ -86,6 +94,7 @@ const TaskList = () => {
           info={info}
           handleEditTask={handleEditTask}
           handleDeleteTask={() => handleDeleteTask(index)}
+          
           index={index}
         />
       ))}
